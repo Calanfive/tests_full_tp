01e345a9861027f1ea0616d96d8a3d1e5d4ab1c5
calanfive <camille.lelan@gmail.com>
2023-11-30 16:35:11 +0100

    getValeur function

--- src/calculerHeure.ts
+++ src/calculerHeure.ts
@@ -1,50 +1,28 @@
 export function getValeur(lune: number, terre: number, soleil: number): number {
+    let result = lune + terre + soleil
     
-    let pouvoir = lune + terre + soleil 
-    let cadran = 0
-    let all = pouvoir + cadran
-
     if (terre === 2 && soleil === 2) {
         return 6
     }
 
-    if (lune === 1) {
-        all -= 2
-        cadran += 1
-        if(soleil === 1) {
-            all -= terre
-            cadran += 1
-            if (terre === 1) {
-                cadran += 1
-                pouvoir += 2 
-            }
-        }
-        else if(soleil === 2){
-            all += 2*terre
-            cadran += 2
+    if(soleil === 1){
+        result = result - terre
+    }
+    else {
+        result += terre
+        if(terre === 1) {
+            result += 2
         }
     }
 
-    if (lune === 1 && terre === 1) {
-        cadran += 2
-        if (soleil === 2) {
-            all += 1
-        }
+    if(lune === 1){
+        result -= 2
+    }
+    else {
+        result /= 2
     }
 
-    if (lune === 2 && soleil === 1 && terre === 1) {
-        cadran += 3
-        terre = 0
-        all /= 2
-    }
-
-    if (lune === 2 && terre === 1 && soleil === 2) {
-        cadran += 5
-        pouvoir += 2*terre + 2
-        all /= 2
-    }
-
-    return all
+    return result
 }
 
 export function calculerHeure(lune: number, terre: number, soleil: number): string {
