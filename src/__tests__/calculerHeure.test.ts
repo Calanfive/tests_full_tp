830eeee0e34ba6d9bf130622386cf9e18108c6ba
calanfive <camille.lelan@gmail.com>
2023-11-30 16:13:26 +0100

    calculHeure

--- src/__tests__/calculerHeure.test.ts
+++ src/__tests__/calculerHeure.test.ts
@@ -1,10 +1,55 @@
 import { describe, expect, test } from "vitest";
-import { calculerHeure } from "../calculerHeure";
+import { calculerHeure, getValeur } from "../calculerHeure";
 
 describe("calculs", () => {
-    describe("total", () => {
-        test("result", () => {
-            expect(calculerHeure(1, 2, 1)).toBe("mortin");
+    // describe("totalByString", () => {
+    //     test("resultA", () => {
+    //         expect(calculerHeure(1, 1, 1)).toBe("mortin");
+    //     })
+    //     test("resultB", () => {
+    //         expect(calculerHeure(1, 2, 2)).toBe("nuight");
+    //     })
+    //     test("resultC", () => {
+    //         expect(calculerHeure(2, 2, 2)).toBe("nuight");
+    //     })
+    //     test("resultD", () => {
+    //         expect(calculerHeure(2, 2, 1)).toBe("aprenoon");
+    //     })
+    //     test("resultE", () => {
+    //         expect(calculerHeure(2, 1, 1)).toBe("aprenoon"); 
+    //     })
+    //     test("resultF", () => {
+    //         expect(calculerHeure(2, 1, 2)).toBe("soirning");
+    //     })
+    //     test("resultG", () => {
+    //         expect(calculerHeure(1, 2, 1)).toBe("mortin");
+    //     })
+    // })
+
+    describe("totalByNumber", () => {
+        test("result1", () => {
+            expect(getValeur(1, 1, 1)).toBe(0);
+        })
+        test("result2", () => {
+            expect(getValeur(1, 1, 2)).toBe(5);
+        })
+        test("result3", () => {
+            expect(getValeur(1, 2, 2)).toBe(6);
+        })
+        test("result4", () => {
+            expect(getValeur(2, 2, 2)).toBe(6);
+        })
+        test("result5", () => {
+            expect(getValeur(2, 2, 1)).toBe(1.5);
+        })
+        test("result6", () => {
+            expect(getValeur(2, 1, 1)).toBe(1.5); 
+        })
+        test("result7", () => {
+            expect(getValeur(2, 1, 2)).toBe(4);
+        })
+        test("result8", () => {
+            expect(getValeur(1, 2, 1)).toBe(0);
         })
     })
-})
+});
--- src/calculerHeure.ts
+++ src/calculerHeure.ts
@@ -1,6 +1,50 @@
 export function getValeur(lune: number, terre: number, soleil: number): number {
-    const result = lune + terre + soleil 
-    return 0
+    
+    let pouvoir = lune + terre + soleil 
+    let cadran = 0
+    let all = pouvoir + cadran
+
+    if (terre === 2 && soleil === 2) {
+        return 6
+    }
+
+    if (lune === 1) {
+        all -= 2
+        cadran += 1
+        if(soleil === 1) {
+            all -= terre
+            cadran += 1
+            if (terre === 1) {
+                cadran += 1
+                pouvoir += 2 
+            }
+        }
+        else if(soleil === 2){
+            all += 2*terre
+            cadran += 2
+        }
+    }
+
+    if (lune === 1 && terre === 1) {
+        cadran += 2
+        if (soleil === 2) {
+            all += 1
+        }
+    }
+
+    if (lune === 2 && soleil === 1 && terre === 1) {
+        cadran += 3
+        terre = 0
+        all /= 2
+    }
+
+    if (lune === 2 && terre === 1 && soleil === 2) {
+        cadran += 5
+        pouvoir += 2*terre + 2
+        all /= 2
+    }
+
+    return all
 }
 
 export function calculerHeure(lune: number, terre: number, soleil: number): string {
