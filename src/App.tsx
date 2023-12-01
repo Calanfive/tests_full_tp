12e9c95fd0f1f7e4f8c77299cac2a706b69e5669
calanfive <camille.lelan@gmail.com>
2023-12-01 10:22:41 +0100

    handlechange + css colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [input1, setInput1] = useState<number>(0)
+  const [input2, setInput2] = useState<number>(0)
+  const [input3, setInput3] = useState<number>(0)
+
+  const handleNameChange1 = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInput1(parseInt(e.target.value));
+}, [input1]);
+
+  const handleNameChange2 = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInput2(parseInt(e.target.value));
+}, [input2]);
+
+  const handleNameChange3 = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInput3(parseInt(e.target.value));
+}, [input3]);
+
+  
+  const handleSubmit = useCallback(async() => {
+
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input className='input' name='lune' value={input1} type="number" onChange={handleNameChange1} />
+        <input className='input' name='terre' value={input2} type="number" onChange={handleNameChange2} />
+        <input className='input' name='soleil' value={input3} type="number" onChange={handleNameChange3} />
+        <button id='button' onClick={handleSubmit}>Calculer</button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
